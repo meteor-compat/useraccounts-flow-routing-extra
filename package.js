63b3cf2b5f6fa8a592de521c3a671bb66943ed69
filipenevola <filipemkv@gmail.com>
2021-11-19 07:38:16 -0400

    first commit

---
+++ package.js
@@ -0,0 +1,44 @@
+// Package metadata for Meteor.js web platform (https://www.meteor.com/)
+// This file is defined within the Meteor documentation at
+//
+//   http://docs.meteor.com/#/full/packagejs
+//
+// and it is needed to define a Meteor package
+'use strict';
+
+Package.describe({
+  name: 'useraccounts:flow-routing-extra',
+  summary: 'UserAccounts package providing routes configuration capability via ostrio:flow-router-extra.',
+  version: '1.0.0',
+  git: 'https://github.com/meteor-compat/useraccounts-flow-routing',
+});
+
+Package.onUse(function(api) {
+  api.versionsFrom('METEOR@2.4');
+
+  api.use('ecmascript');
+
+  api.use([
+    'check',
+    'ostrio:flow-router-extra',
+    'underscore',
+    'useraccounts:core',
+    'modules'
+  ], ['client', 'server']);
+
+  api.use([
+     'react@0.14.1_1',
+     'kadira:blaze-layout@2.3.0',
+     'kadira:react-layout@1.5.3',
+     'gwendall:blaze-to-react@0.1.2'
+  ], ['client', 'server'], { weak: true });
+
+  api.addFiles([
+    'lib/core.js',
+  ], ['client', 'server']);
+
+  api.addFiles([
+    'lib/client/client.js',
+    'lib/client/templates_helpers/at_input.js',
+  ], ['client']);
+});
